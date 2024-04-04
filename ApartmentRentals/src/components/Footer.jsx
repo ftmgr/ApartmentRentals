30e96006dbc607874289c7a3045ad875b576a313
Fatma <fatmagurses1@gmail.com>
2024-04-04 11:36:09 +0200

    Getting started

---
+++ ApartmentRentals/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <p>&copy; 2024 Your Company</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ ApartmentRentals/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            <aside>
+                <ul>
+                    <li>Search</li>
+                    <li>Filter</li>
+                    <li>Sort</li>
+                </ul>
+            </aside>
+        </div>
+    );
+};
+
+export default Sidebar;
